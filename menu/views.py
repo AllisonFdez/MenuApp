d3aae03a59bec2c525f3e0e281a56416420ff62d
Allison Fdez <mpardo@uninorte.edu.co>
2017-11-19 02:46:45 -0500

    Sign in and log in

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http 	  import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from menu.forms import SigninForm
+
 
 # Create your views here.
 
@@ -11,3 +17,18 @@ def index(request):
 
 def menu(request):
 	return render(request, 'menu.html',{})
+
+def login(request):
+	return render(request, 'login.html',{})
+
+def signin(request):
+	if request.method =="POST":
+		form = SigninForm(request.POST)
+		if form.is_valid():
+		   form.save()
+		   return redirect('/menu')
+	else:
+		form = SigninForm()
+
+	return render(request,'signin.html',{'form': form})
+		
